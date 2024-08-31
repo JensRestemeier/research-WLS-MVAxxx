df4c737d1a4e8418e5d36c66fd6f0b073ea20302
Jens Ch. Restemeier <jens.restemeier@gmail.com>
2024-08-31 17:32:48 +0200

    implemented scanning for energy monitor devices

--- host.py
+++ host.py
@@ -4,6 +4,10 @@ from bleak import BleakScanner
 from bleak import BleakClient
 from bleak import uuids
 
+uart_uuid = uuids.normalize_uuid_16(0xFFF0)
+uart_receive_uuid = uuids.normalize_uuid_16(0xFFF1)
+uart_write_uuid = uuids.normalize_uuid_16(0xFFF2)
+
 def calc_crc(message):
     crc = 255
     for x in message:
@@ -11,44 +15,33 @@ def calc_crc(message):
     crc &= 0xFF
     return crc
 
-"""
-52C2E87B-536B-2008-4047-36041D4415B4: CH9141BLE2U
-AdvertisementData(local_name='device ', manufacturer_data={48652: b'1\x00\x00\x00\x00\x00'}, tx_power=0, rssi=-47)
-"""
-
-"""
-PNP ID: Hex:		01390700001001
-System Id: Hex:		13DE79000010535C
-"""
-
-async def main():
-    uart_uuid = uuids.normalize_uuid_16(0xFFF0)
-    uart_receive_uuid = uuids.normalize_uuid_16(0xFFF1)
-    uart_write_uuid = uuids.normalize_uuid_16(0xFFF2)
-
-    energy_monitors = []
+async def listDevices():
     devices = await BleakScanner.discover(return_adv=True)
     for device, adv in devices.values():
-        print (device)
-        print (adv)
-        if uart_uuid in adv.service_uuids:
-            print (f"potential energy monitor {adv.local_name}")
-
-            # try to receive a message and check the header and checksum are correct:
+        # The manufacturer ID seems to change randomly, so let's just look at the data:
+        values = list(adv.manufacturer_data.values())
+        manufacturer_data = values[0] if len(values) > 0 else bytes()
+        if manufacturer_data == bytes([0x31,0x00,0x00,0x00,0x00,0x00]):
             async with BleakClient(device) as client:
-                pnp_id = await client.read_gatt_char("2a50")
-                print("PNP ID: %s" % "".join(["%2.2x" % x for x in pnp_id]))
+                # Have a look if we have a UART channel:
+                hasUartChannel = False
+                for service in client.services:
+                    if service.uuid == uart_uuid:
+                        for char in service.characteristics:
+                            if char.uuid == uart_receive_uuid and "read" in char.properties:
+                                hasUartChannel = True
 
-                system_id = await client.read_gatt_char("2a23")
-                print("System ID: %s" % "".join(["%2.2x" % x for x in system_id]))
+                if hasUartChannel:
+                    # try to receive a message and check the header and checksum are correct:
+                    message = await client.read_gatt_char(uart_receive_uuid)
+                    # print ("".join(["%2.2x" % x for x in message]))
 
-                message = await client.read_gatt_char(uart_receive_uuid)
-                print ("".join(["%2.2x" % x for x in message]))
+                    if struct.unpack_from(">H", message, 0)[0] == 0xB55B:
+                        crc = calc_crc(message[0:len(message)-1])
+                        if crc == message[-1]:
+                            print (device)
 
-                if struct.unpack_from(">H", message, 0)[0] == 0xB55B:
-                    crc = calc_crc(message[0:len(message)-1])
-                    if crc == message[-1]:
-                        print ("energy monitor found!")
-                        energy_monitors.append(device)
+async def main():
+    await listDevices()
 
 asyncio.run(main())
